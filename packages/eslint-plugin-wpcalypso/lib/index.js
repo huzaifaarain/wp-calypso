ac08dc471e1b07486b2742f8aaf6578bddf30183
Jon Surrell <jon.surrell@automattic.com>
2018-12-04 22:50:06 +0000

    Simplify deps

--- packages/eslint-plugin-wpcalypso/lib/index.js
+++ packages/eslint-plugin-wpcalypso/lib/index.js
@@ -8,14 +8,8 @@
 'use strict';
 
 //------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
-var requireIndex = require( 'requireindex' );
-
-//------------------------------------------------------------------------------
 // Plugin Definition
 //------------------------------------------------------------------------------
 
 // import all rules in lib/rules
-module.exports.rules = requireIndex( __dirname + '/rules' );
+module.exports.rules = require( './rules' );
